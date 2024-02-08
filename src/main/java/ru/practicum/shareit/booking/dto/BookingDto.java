f8bab6d7204092ab4c17a22a9bb56a1ea0ad2b58
Gilborz <gilborz2018@yandex.ru>
2024-02-08 14:12:23 +0300

    second

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,11 +1,13 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class BookingDto {
     private LocalDateTime start;
 
